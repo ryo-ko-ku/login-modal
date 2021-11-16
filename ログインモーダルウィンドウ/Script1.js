448821f54121d54039be24c0704520584261c9f2
ryo-ko-ku <69999863+ryo-ko-ku@users.noreply.github.com>
2021-11-16 11:10:10 +0900

    Delete ログインモーダルウィンドウ directory

--- ログインモーダルウィンドウ/Script1.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-$(function () {
-    $('.js-open').click(function () {
-        $('#overlay, .modal-window').fadeIn();
-    });
-    $('.js-close,#overlay').click(function () {
-        $('#overlay, .modal-window').fadeOut();
-    });
-});
